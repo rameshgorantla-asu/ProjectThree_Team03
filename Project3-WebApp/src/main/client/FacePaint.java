13c551b50a332b9497b792fc57e490fd2205099f
Yathartha Goel <ygoel@asu.edu>
2018-04-01 16:24:07 -0700

    Added all the expressions required

--- Project3-WebApp/src/main/client/FacePaint.java
+++ Project3-WebApp/src/main/client/FacePaint.java
@@ -3,6 +3,11 @@ package client;
 import java.applet.Applet;
 import java.awt.*;
 
+/**
+ * Class to draw a face and represent the facial expressions.
+ * @SER516 Project3_Team03
+ * @version 1.0
+ */
 public class FacePaint extends Applet {
 	Graphics offscreenContext;
     Image offscreenImage;
@@ -30,24 +35,33 @@ public class FacePaint extends Applet {
 	
 	getVectors v = new getVectors();
 	
+	/**
+	 * Draws the face with the required expressions
+	 * @param g Is a Graphics Object
+	 * @param v Contains the array of expressions
+	 * @param x Specifies the starting point on the x axis
+	 * @param y Specifies the starting point on the y axis
+	 * @param height Specifies the height of the window
+	 * @param width Specifies the width of the window
+	 */
 	public void drawFace(Graphics g, getVectors v, int x, int y, int height, int width){
 		String direction = "Center";
-		int blinkl = 0;
-		int blinkr = 0;
-		if(v.p[11] == 1){ 
-			blinkl = 1;
-			blinkr = 1;
+		boolean blinkl = false;
+		boolean blinkr = false;
+		if(v.p[8] == 1){ 
+			blinkl = true;
+			blinkr = true;
 		}
-		else if(v.p[12] == 1){ 
-			blinkl = 1;
+		else if(v.p[9] == 1){ 
+			blinkl = true;
 		}
-		else if(v.p[13] == 1){ 
-			blinkr = 1;
+		else if(v.p[10] == 1){ 
+			blinkr = true;
 		}
-		else if(v.p[14] == 1){
+		else if(v.p[11] == 1){
 			direction = "Left";
 		}
-		else if(v.p[15] == 1){
+		else if(v.p[12] == 1){
 			direction = "Right";
 		}
 		calc_scaleFactors(x, y, height, width);
@@ -55,10 +69,17 @@ public class FacePaint extends Applet {
 		make_eye(g, blinkl, blinkr);
 		make_pupil(g, direction, blinkl, blinkr);
 		make_eyebrows(g, v.p[1], v.p[2]);
-		make_nose(g, v.p[5]);
-		make_mouth(g, v.p[6], v.p[9], v.p[10]);
+		make_nose(g);
+		make_mouth(g, v.p[3], v.p[4] , v.p[5], v.p[6], v.p[7]);
 	}
 	
+	/**
+	 * Finds the scaling factors so as to scale the face according to the background.
+	 * @param x Specifies the starting point on the x axis
+	 * @param y Specifies the starting point on the y axis
+	 * @param height Specifies the height of the window
+	 * @param width Specifies the width of the window
+	 */
 	public void calc_scaleFactors(int x, int y, int height, int width){
 		x_factor = width / 100.0;
 		y_factor = height / 100.0;
@@ -66,20 +87,30 @@ public class FacePaint extends Applet {
 		y_origin = y;
 	}
 	
+	/**
+	 * Makes outer structure of the face
+	 * @param g Is a Graphics Object
+	 */
 	public void make_head(Graphics g){
 		createCircle(g, 50, 50, head_radiusval);
 	}
 	
-	public void make_eye(Graphics g, int bl, int br){
-		if(bl == 1 && br == 1){
+	/**
+	 * Makes the eyes along with the various expressions needed
+	 * @param g Is a Graphics Object
+	 * @param bl Contains value for blinking of the left eye
+	 * @param br Contains value for blinking of the right eye
+	 */
+	public void make_eye(Graphics g, boolean bl, boolean br){
+		if(bl == true && br == true){
 			createLine(g, eye_left_xpos + eye_radiusval, eye_ypos, eye_left_xpos - eye_radiusval, eye_ypos);
 			createLine(g, eye_right_xpos - eye_radiusval, eye_ypos, eye_right_xpos + eye_radiusval, eye_ypos);
 		}
-		else if(bl == 1){
+		else if(bl == true){
 			createLine(g, eye_left_xpos + eye_radiusval, eye_ypos, eye_left_xpos - eye_radiusval, eye_ypos);
 			createOval(g, eye_right_xpos, eye_ypos, eye_radiusval, eye_radiusval);
 		}
-		else if(br == 1){
+		else if(br == true){
 			createOval(g, eye_left_xpos, eye_ypos, eye_radiusval, eye_radiusval);
 			createLine(g, eye_right_xpos - eye_radiusval, eye_ypos, eye_right_xpos + eye_radiusval, eye_ypos);
 		}
@@ -89,7 +120,14 @@ public class FacePaint extends Applet {
 		}
 	}
 	
-	public void make_pupil(Graphics g, String direction, int bl, int br){
+	/**
+	 * Makes the pupils along with the various expressions needed.
+	 * @param g Is a Graphics Object
+	 * @param direction Contains the direction values for looking left or right
+	 * @param bl Contains value for blinking of the left eye
+	 * @param br Contains value for blinking of the right eye
+	 */
+	public void make_pupil(Graphics g, String direction, boolean bl, boolean br){
 		double d = 0.5;
 		int size = 2;
 		if(direction.equals("Right")){
@@ -103,29 +141,36 @@ public class FacePaint extends Applet {
 			fillOval(g, eye_right_xpos - (int)((d - 0.5) * 10), eye_ypos, size, size);
 		}
 		else{
-			if(bl == 1){
+			if(bl == true){
 				fillOval(g, eye_right_xpos + (int)((d - 0.5) * 10), eye_ypos, size, size);
 			}
-			else if(br == 1){
+			else if(br == true){
 				fillOval(g, eye_left_xpos - (int)((d - 0.5) * 10), eye_ypos, size, size);
 			}
-			else if(bl == 0 && br == 0){
+			else if(bl == true && br == true){
 				fillOval(g, eye_left_xpos - (int)((d - 0.5) * 10), eye_ypos, size, size);
 				fillOval(g, eye_right_xpos + (int)((d - 0.5) * 10), eye_ypos, size, size);
 			}
 		}
 	}
 	
+	/**
+	 * Makes the eyebrows along with the various expressions needed.
+	 * @param g Is a Graphics Object
+	 * @param p1 Raises the brows according to this input
+	 * @param p2 Furrows the brows according to this input
+	 */
 	public void make_eyebrows(Graphics g, double p1, double p2){
-		p1 = 0;
+		p1=0.0;
+		p2=0.0;
 		int y1, y2;
-		if(p1 != 0 ){
-			y1 = eyebrow_y + (int)(p1 * 10);
-			y2 = eyebrow_y - (int)(p1 * 10);
+		if(p1 != 0.0 ){
+			y1 = eyebrow_y + (int)(p1 * 5);
+			y2 = eyebrow_y - (int)(p1 * 5);
 		}
-		else if(p2 != 0){
-			y1 = eyebrow_y - (int)(p2 * 10);
-			y2 = eyebrow_y + (int)(p2 * 10);
+		else if(p2 != 0.0 ){
+			y1 = eyebrow_y - (int)(p2 * 5);
+			y2 = eyebrow_y + (int)(p2 * 5);
 		}
 		else{
 			y1 = eyebrow_y;
@@ -135,26 +180,80 @@ public class FacePaint extends Applet {
 		createLine(g, eyebrow_right_left_x, y2, eyebrow_right_right_x, y1);
 	}
 	
-	public void make_nose(Graphics g, double p5){
-		int y = 55 + (int)(((p5 - 0.5) / 2.0) * 10);
+	/**
+	 * Makes the nose.
+	 * @param g Is a Graphics Object
+	 */
+	public void make_nose(Graphics g){
+		int y = 54;
 		createLine(g, nose_apex_x, nose_apex_y, nose_apex_x - (nose_width / 2), y);
 		createLine(g, nose_apex_x - (nose_width / 2), y, nose_apex_x + (nose_width / 2), y);
 		createLine(g, nose_apex_x + (nose_width / 2), y, nose_apex_x, nose_apex_y);
 	}
 	
-	public void make_mouth(Graphics g, double p6, double p9, double p10){
-		double size = ((p9 - 0.5) * 10);
-		double x1 = 40 - size;
+	/**
+	 * Makes the mouth along with the expressions needed.
+	 * @param g Is a Graphics Object
+	 * @param p3 Contains input for making the face smile
+	 * @param p4 Contains input to make the face clench
+	 * @param p5 Contains input for making the face smirk left
+	 * @param p6 Contains input for making the face smirk right
+	 * @param p7 Contains input for making the face laugh
+	 */
+	public void make_mouth(Graphics g, double p3, double p4, double p5, double p6, double p7){
+		double x1 = 40;
 		double y1 = mouth_ypos;
-		double x2 = 60 + size;
+		double x2 = 60;
 		double y2 = mouth_ypos;
 		double x3 = ((x2 - x1) / 2) + x1;
-		double y3 = ((p6 - 0.5) * 10) + mouth_ypos;
-
+		double y3 = mouth_ypos;
+		double y3lower = mouth_ypos;
+		p3 = 0.0;
+		p4 = 0.0;
+		p5 = 0.0;
+		p6 = 0.0;
+		p7 = 0.0;
+		if(p3 > 0.0){
+			x1 = x1 - (p3 * 5);
+			x2 = x2 + (p3 * 5);
+			y3 = y3 + ((p3 / 2.0) * 10);
+			y3lower = y3;
+		}
+		if(p4 > 0.0){
+			x1 = x1 - (p4 * 5);
+			x2 = x2 + (p4 * 5);
+			y3 = y3 - ((p4 / 2.0) * 10);
+			y3lower = y3lower + ((p4 / 2.0) * 10);
+		}
+		if(p5 > 0.0){
+			x1 = x1 + (p5 * 3);
+			x2 = x2 + (p5 * 3);
+			y2 = y2 - (p5 * 5);
+		}
+		if(p6 > 0.0){
+			x1 = x1 - (p6 * 3);
+			x2 = x2 - (p6 * 3);
+			y1 = y1 - (p6 * 5);
+		}
+		if(p7 > 0.0){
+			x1 = x1 - (p7 * 5);
+			x2 = x2 + (p7 * 5);
+			y3lower = y3lower + ((p7 / 2.0) * 10);
+		}
 		make_lips(g, x1, y1, x2, y2, x3, y3);
-		make_lips(g, x1, y1, x2, y2, x3, y3 + ((p10 / 2.0) * 10));
+		make_lips(g, x1, y1, x2, y2, x3, y3lower);
 	}
 	
+	/**
+	 * Used to make the lips
+	 * @param g Is a Graphics Object
+	 * @param x1 Contains the starting point of the lips on the horizontal plane
+	 * @param y1 Contains the starting point of the lips on the vertical plane
+	 * @param x2 Contains the ending point of the lips on the horizontal plane
+	 * @param y2 Contains the ending point of the lips on the vertical plane
+	 * @param x3 Contains the center point of the lips on the horizontal plane
+	 * @param y3 Contains the center point of the lips on the vertical plane
+	 */
 	public void make_lips(Graphics g, double x1, double y1, double x2, double y2, double  x3, double y3){
 		int i, new_x, new_y, last_x, last_y;
 		double denom = (Math.pow(x1, 2) * (x2 - x3)) + (x1 * (Math.pow(x3, 2) - Math.pow(x2, 2))) 
@@ -178,38 +277,87 @@ public class FacePaint extends Applet {
 		}
 	}
 	
+	/**
+	 * Creates a hollow Circle
+	 * @param g Is a Graphics Object
+	 * @param x Contains the x position with respect to the background
+	 * @param y Contains the y position with respect to the background
+	 * @param radius Contains the radius of the circle to be drawn
+	 */
 	public void createCircle(Graphics g, int x, int y, int radius){
 		g.drawOval(scale_x(x - radius) + x_origin, scale_y(y - radius) + y_origin,
 		           scale_x(radius * 2), scale_y(radius * 2));
 	}
+	
+	/**
+	 * Creates a hollow Oval
+	 * @param g Is a Graphics Object
+	 * @param x Contains the x position with respect to the background
+	 * @param y Contains the y position with respect to the background
+	 * @param height Contains the height to the oval to be drawn
+	 * @param width Contains the width of the oval to be drawn
+	 */
 	public void createOval(Graphics g, int x, int y, int height, int width){
 		g.drawOval(scale_x(x - width) + x_origin, scale_y(y - height) + y_origin,
 				   scale_x(width * 2), scale_y(height * 2));
 	}
 	
+	/**
+	 * Used to fill the hollow Oval
+	 * @param g Is a Graphics Object
+	 * @param x Contains the x position with respect to the background
+	 * @param y Contains the y position with respect to the background
+	 * @param height Contains the height till which the oval to be filled
+	 * @param width Contains the width till which the oval to be filled
+	 */
 	public void fillOval(Graphics g, int x, int y, int height, int width){
 		g.fillOval(scale_x(x - width) + x_origin, scale_y(y - height) + y_origin,
 				   scale_x(width * 2), scale_y(height * 2) );
 	}
 	
+	/**
+	 * Used to create a line
+	 * @param g Is a Graphics Object
+	 * @param x1 Contains the horizontal starting position of the line
+	 * @param y1 Contains the vertical starting position of the line
+	 * @param x2 Contains the horizontal ending position of the line
+	 * @param y2 Contains the vertical ending position of the line
+	 */
 	public void createLine(Graphics g, int x1, int y1, int x2, int y2){
 		g.drawLine(scale_x(x1) + x_origin, scale_y(y1) + x_origin,
 				 scale_x(x2) + x_origin, scale_y(y2) + x_origin);
 	}
+	
+	/**
+	 * Used to scale the input according to the horizontal factor of the background
+	 * @param x Parameter needed to be scaled
+	 * @return Scaled parameter
+	 */
 	public int scale_x(int x) {
 		return (int)(x * x_factor);
 	}
-
+	
+	/**
+	 * Used to scale the input according to the vertical factor of the background
+	 * @param y Parameter needed to be scaled
+	 * @return Scaled parameter
+	 */
 	public int scale_y(int y) {
 		return (int)(y * y_factor);
 	}
 	
+	/**
+	 * Initializes the applet.
+	 */
 	public void init(){
 		offscreenImage = this.createImage(getSize().width, getSize().height);
         offscreenContext = offscreenImage.getGraphics();
         backgroundcolor = getBackground();
 	}
 	
+	/**
+	 * Paints the desired face.
+	 */
 	public void paint(Graphics g){
 		drawFace(g, v, 0, 0, getSize().height, getSize().width);
 	}
--- Project3-WebApp/src/main/client/getVectors.java
+++ Project3-WebApp/src/main/client/getVectors.java
@@ -1,31 +1,49 @@
 package client;
-
-
 import java.util.Random;
 
+/**
+ * Class to transfer the received expressions into an array. 
+ * @SER516 Project3_Team03
+ * @version 1.0
+ */
 public class getVectors {
-    public double p[] = new double[16];
-	
-	public getVectors(double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8, double p9, double p10) {
+    public double p[] = new double[13];
+    
+    /**
+     * Puts all the received expression inputs into an array
+     * @param p1 contains input to raise brow
+     * @param p2 contains input to furrow brow
+     * @param p3 contains input to smile
+     * @param p4 contains input to clench
+     * @param p5 contains input to smirk left
+     * @param p6 contains input to smirk right
+     * @param p7 contains input to laugh
+     * @param p8 contains input to blink the eyes
+     * @param p9 contains input to wink left eye
+     * @param p10 contains input to wink right eye
+     * @param p11 contains input to look left
+     * @param p12 contains input to look right
+     */
+    public getVectors(double p1, double p2, double p3, double p4, double p5, double p6, double p7, double p8, double p9, double p10, double p11, double p12) {
         p[1] = p1; p[2] = p2; p[3] = p3; p[4] = p4; p[5] = p5;
         p[6] = p6; p[7] = p7; p[8] = p8; p[9] = p9; p[10] = p10;
+        p[11] = p11; p[12] = p12;
     }
     
     public getVectors() {
         int i;
-        Random r = new Random();	
-        for (i = 1; i < 16; i++) {
+        Random r = new Random();    
+        for (i = 1; i < 13; i++) {
             p[i] = r.nextDouble();
             System.out.println(p[i]);
         }
-        p[12] = 1;
     }
     
 //    public double distance(getVectors v) {
 //        int i;
 //        double sum = 0.0;
 //        double diff;
-//	
+//  
 //        for (i = 1; i < 11; i++) {
 //            diff = p[i] - v.p[i];
 //            sum = sum + diff * diff;

23cb44755f6ebf6afbb640d809ae5194b0e94224
U-ASURITE\rgorantl <rgorantl@OKED2671.asurite.ad.asu.edu>
2018-04-03 16:48:09 -0700

    created server model

---
+++ Project3-WebApp/src/main/java/server/FaceAffectiveData.java
@@ -0,0 +1,35 @@
+package server;
+
+public class FaceAffectiveData {
+	Double meditation;
+	Double engagementBoredom;
+	Double excitementShortTerm;
+	Double excitementLongTerm;
+	
+	public Double getMeditation() {
+		return meditation;
+	}
+	public void setMeditation(Double meditation) {
+		this.meditation = meditation;
+	}
+	public Double getEngagementBoredom() {
+		return engagementBoredom;
+	}
+	public void setEngagementBoredom(Double engagementBoredom) {
+		this.engagementBoredom = engagementBoredom;
+	}
+	public Double getExcitementShortTerm() {
+		return excitementShortTerm;
+	}
+	public void setExcitementShortTerm(Double excitementShortTerm) {
+		this.excitementShortTerm = excitementShortTerm;
+	}
+	public Double getExcitementLongTerm() {
+		return excitementLongTerm;
+	}
+	public void setExcitementLongTerm(Double excitementLongTerm) {
+		this.excitementLongTerm = excitementLongTerm;
+	}
+	
+	
+}
---
+++ Project3-WebApp/src/main/java/server/FaceExpressionData.java
@@ -0,0 +1,83 @@
+package server;
+
+public class FaceExpressionData {
+	Double lookingRight;
+	Double eyebrowRaise;
+	Double lookingLeft;
+	Double LookingUp;
+	Double lookingDown;
+	Double rightWink;
+	Double leftWink;
+	Double blink;
+	Double eyesOpen;
+	Double smile;
+	Double clench;
+	public Double getLookingRight() {
+		return lookingRight;
+	}
+	public void setLookingRight(Double lookingRight) {
+		this.lookingRight = lookingRight;
+	}
+	public Double getEyebrowRaise() {
+		return eyebrowRaise;
+	}
+	public void setEyebrowRaise(Double eyebrowRaise) {
+		this.eyebrowRaise = eyebrowRaise;
+	}
+	public Double getLookingLeft() {
+		return lookingLeft;
+	}
+	public void setLookingLeft(Double lookingLeft) {
+		this.lookingLeft = lookingLeft;
+	}
+	public Double getLookingUp() {
+		return LookingUp;
+	}
+	public void setLookingUp(Double lookingUp) {
+		LookingUp = lookingUp;
+	}
+	public Double getLookingDown() {
+		return lookingDown;
+	}
+	public void setLookingDown(Double lookingDown) {
+		this.lookingDown = lookingDown;
+	}
+	public Double getRightWink() {
+		return rightWink;
+	}
+	public void setRightWink(Double rightWink) {
+		this.rightWink = rightWink;
+	}
+	public Double getLeftWink() {
+		return leftWink;
+	}
+	public void setLeftWink(Double leftWink) {
+		this.leftWink = leftWink;
+	}
+	public Double getBlink() {
+		return blink;
+	}
+	public void setBlink(Double blink) {
+		this.blink = blink;
+	}
+	public Double getEyesOpen() {
+		return eyesOpen;
+	}
+	public void setEyesOpen(Double eyesOpen) {
+		this.eyesOpen = eyesOpen;
+	}
+	public Double getSmile() {
+		return smile;
+	}
+	public void setSmile(Double smile) {
+		this.smile = smile;
+	}
+	public Double getClench() {
+		return clench;
+	}
+	public void setClench(Double clench) {
+		this.clench = clench;
+	}
+	
+	
+}

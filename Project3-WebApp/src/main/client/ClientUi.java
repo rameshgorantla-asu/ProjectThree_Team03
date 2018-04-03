6b36aee9867e82b18eda115f7abe88664012eb31
Srajan Gupta <sgpt18@gmail.com>
2018-04-02 22:13:57 -0700

    Implemented 12 Graphs
    Graphs are plotted for random values for the facial expressions in Expressive tab.

--- Project3-WebApp/src/main/client/ClientUi.java
+++ Project3-WebApp/src/main/client/ClientUi.java
@@ -19,6 +19,10 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.BorderFactory;
 import javax.swing.border.LineBorder;
 
 /**
@@ -84,15 +88,33 @@ public class ClientUi {
 		facePanel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		facePanel.setBounds(0, 28, 326, 313);
 		expressivePanel.add(facePanel);
-		
+                
+                FacePaint f = new FacePaint();
+                DisplayGraph g = new DisplayGraph();
+		DisplayGraph g1 = new DisplayGraph();
+                DisplayGraph g2 = new DisplayGraph();
+		DisplayGraph g3 = new DisplayGraph();
+                DisplayGraph g4= new DisplayGraph();
+		DisplayGraph g5 = new DisplayGraph();
+                DisplayGraph g6 = new DisplayGraph();
+		DisplayGraph g7 = new DisplayGraph();
+                DisplayGraph g8 = new DisplayGraph();
+		DisplayGraph g9 = new DisplayGraph();
+                DisplayGraph g10 = new DisplayGraph();
+		DisplayGraph g11 = new DisplayGraph();
+	
 		JPanel graphPanel = new JPanel();
 		graphPanel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
 		graphPanel.setBounds(325, 28, 368, 313);
 		expressivePanel.add(graphPanel);
 		graphPanel.setLayout(null);
 		
-		FacePaint f = new FacePaint();
-		
+                JPanel blinkGraph = new JPanel();
+		blinkGraph.setBounds(90, 11, 260, 18);
+		blinkGraph.setBackground(ClientConstants.GRAPHBG);
+		blinkGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(blinkGraph);
+                
 		JLabel blinkLabel = new JLabel("Blink\r\n");
 		blinkLabel.setForeground(Color.WHITE);
 		blinkLabel.setOpaque(true);
@@ -100,6 +122,12 @@ public class ClientUi {
 		blinkLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		blinkLabel.setBounds(10, 11, 68, 20);
 		graphPanel.add(blinkLabel);
+                
+                JPanel rgtWinkGraph = new JPanel();
+		rgtWinkGraph.setBounds(90, 36, 260, 18);
+		rgtWinkGraph.setBackground(ClientConstants.GRAPHBG);
+		rgtWinkGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(rgtWinkGraph);
 		
 		JLabel rgtWinkLabel = new JLabel("Right Wink");
 		rgtWinkLabel.setBackground(Color.GRAY);
@@ -108,6 +136,12 @@ public class ClientUi {
 		rgtWinkLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		rgtWinkLabel.setBounds(10, 36, 68, 18);
 		graphPanel.add(rgtWinkLabel);
+                
+                JPanel lftWinkGraph = new JPanel();
+		lftWinkGraph.setBounds(90, 65, 260, 18);
+		lftWinkGraph.setBackground(ClientConstants.GRAPHBG);
+		lftWinkGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(lftWinkGraph);
 		
 		JLabel lftWinkLabel = new JLabel("Left Wink");
 		lftWinkLabel.setOpaque(true);
@@ -117,6 +151,12 @@ public class ClientUi {
 		lftWinkLabel.setBounds(10, 65, 68, 20);
 		graphPanel.add(lftWinkLabel);
 		
+                JPanel lookGraph = new JPanel();
+		lookGraph.setBounds(90, 90, 260, 18);
+		lookGraph.setBackground(ClientConstants.GRAPHBG);
+		lookGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(lookGraph);
+                
 		JLabel lookLabel = new JLabel("Look R/L");
 		lookLabel.setOpaque(true);
 		lookLabel.setForeground(Color.WHITE);
@@ -125,6 +165,12 @@ public class ClientUi {
 		lookLabel.setBounds(10, 90, 68, 20);
 		graphPanel.add(lookLabel);
 		
+                JPanel furrowGraph = new JPanel();
+		furrowGraph.setBounds(90, 115, 260, 18);
+		furrowGraph.setBackground(ClientConstants.GRAPHBG);
+		furrowGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(furrowGraph);
+                
 		JLabel furrowLabel = new JLabel("Furrow Brow\r\n");
 		furrowLabel.setOpaque(true);
 		furrowLabel.setForeground(Color.WHITE);
@@ -132,6 +178,12 @@ public class ClientUi {
 		furrowLabel.setHorizontalAlignment(SwingConstants.LEFT);
 		furrowLabel.setBounds(10, 115, 68, 20);
 		graphPanel.add(furrowLabel);
+                
+                JPanel raiseGraph = new JPanel();
+		raiseGraph.setBounds(90, 140, 260, 18);
+		raiseGraph.setBackground(ClientConstants.GRAPHBG);
+		raiseGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(raiseGraph);
 		
 		JLabel raiseLabel = new JLabel("Raise Brow");
 		raiseLabel.setOpaque(true);
@@ -139,6 +191,12 @@ public class ClientUi {
 		raiseLabel.setBackground(Color.GRAY);
 		raiseLabel.setBounds(10, 140, 68, 20);
 		graphPanel.add(raiseLabel);
+                
+                JPanel smileGraph = new JPanel();
+		smileGraph.setBounds(90, 165, 260, 18);
+		smileGraph.setBackground(ClientConstants.GRAPHBG);
+		smileGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(smileGraph);
 		
 		JLabel smileLabel = new JLabel("Smile");
 		smileLabel.setOpaque(true);
@@ -146,6 +204,12 @@ public class ClientUi {
 		smileLabel.setBackground(Color.GRAY);
 		smileLabel.setBounds(10, 165, 68, 20);
 		graphPanel.add(smileLabel);
+                
+                JPanel clenchGraph = new JPanel();
+		clenchGraph.setBounds(90, 190, 260, 18);
+		clenchGraph.setBackground(ClientConstants.GRAPHBG);
+		clenchGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(clenchGraph);
 		
 		JLabel clenchLabel = new JLabel("Clench");
 		clenchLabel.setOpaque(true);
@@ -153,6 +217,12 @@ public class ClientUi {
 		clenchLabel.setBackground(Color.GRAY);
 		clenchLabel.setBounds(10, 190, 68, 20);
 		graphPanel.add(clenchLabel);
+                
+                JPanel lftSmirkGraph = new JPanel();
+		lftSmirkGraph.setBounds(90, 215, 260, 18);
+		lftSmirkGraph.setBackground(ClientConstants.GRAPHBG);
+		lftSmirkGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(lftSmirkGraph);
 		
 		JLabel lftSmirkLabel = new JLabel("Left Smirk");
 		lftSmirkLabel.setOpaque(true);
@@ -160,6 +230,12 @@ public class ClientUi {
 		lftSmirkLabel.setBackground(Color.GRAY);
 		lftSmirkLabel.setBounds(10, 215, 68, 20);
 		graphPanel.add(lftSmirkLabel);
+                
+                JPanel rgtSmirkGraph = new JPanel();
+		rgtSmirkGraph.setBounds(90, 245, 260, 18);
+		rgtSmirkGraph.setBackground(ClientConstants.GRAPHBG);
+		rgtSmirkGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(rgtSmirkGraph);
 		
 		JLabel rgtSmirkLabel = new JLabel("Right Smirk");
 		rgtSmirkLabel.setOpaque(true);
@@ -167,6 +243,12 @@ public class ClientUi {
 		rgtSmirkLabel.setBackground(Color.GRAY);
 		rgtSmirkLabel.setBounds(10, 245, 68, 20);
 		graphPanel.add(rgtSmirkLabel);
+                
+                JPanel laughGraph = new JPanel();
+		laughGraph.setBounds(90, 276, 260, 18);
+		laughGraph.setBackground(ClientConstants.GRAPHBG);
+		laughGraph.setBorder(BorderFactory.createLineBorder(Color.black));
+                graphPanel.add(laughGraph);
 		
 		JLabel laughLabel = new JLabel("Laugh");
 		laughLabel.setOpaque(true);
@@ -277,6 +359,52 @@ public class ClientUi {
                     
                     facePanel.add(f,BorderLayout.CENTER);
                     facePanel.repaint();
+                    List<Float> al = new ArrayList<Float>(); 
+                    Random rand = new Random();
+                    float pick;
+                    for (int j = 0; j < 1; j++) {
+                        pick = rand.nextInt(100);
+                        al.add(pick);
+                    }
+                    g.addValues(al);
+                    g1.addValues(al);
+                    g2.addValues(al);
+                    g3.addValues(al);
+                    g4.addValues(al);
+                    g5.addValues(al);
+                    g6.addValues(al);
+                    g7.addValues(al);
+                    g8.addValues(al);
+                    g9.addValues(al);
+                    g10.addValues(al);
+                    g11.addValues(al);
+                    
+                    blinkGraph.add(g,BorderLayout.CENTER);
+                    blinkGraph.repaint();
+                    rgtWinkGraph.add(g1,BorderLayout.CENTER);
+                    rgtWinkGraph.repaint();
+                    lftWinkGraph.add(g2,BorderLayout.CENTER);
+                    lftWinkGraph.repaint();
+                    rgtWinkGraph.add(g3,BorderLayout.CENTER);
+                    rgtWinkGraph.repaint();
+                    lookGraph.add(g4,BorderLayout.CENTER);
+                    lookGraph.repaint();
+                    furrowGraph.add(g5,BorderLayout.CENTER);
+                    furrowGraph.repaint();
+                    raiseGraph.add(g6,BorderLayout.CENTER);
+                    raiseGraph.repaint();
+                    smileGraph.add(g7,BorderLayout.CENTER);
+                    smileGraph.repaint();
+                    clenchGraph.add(g8,BorderLayout.CENTER);
+                    clenchGraph.repaint();
+                    lftSmirkGraph.add(g9,BorderLayout.CENTER);
+                    lftSmirkGraph.repaint();
+                    rgtSmirkGraph.add(g10,BorderLayout.CENTER);
+                    rgtSmirkGraph.repaint();
+                    laughGraph.add(g11,BorderLayout.CENTER);
+                    laughGraph.repaint();
+                    
+                    //System.out.println(al);
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
